f5b4b55db863ce43a2bf224c2e06bba13ad48b85
Roberto Barros <roberto.f.barros@gmail.com>
2026-04-13 17:35:40 -0300

    Release v0.1.6

--- Formula/lazyrspec.rb
+++ Formula/lazyrspec.rb
@@ -1,9 +1,9 @@
 class Lazyrspec < Formula
   desc "A lazy TUI for running RSpec tests"
   homepage "https://github.com/RobertoBarros/lazyrspec"
-  url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.5/lazyrspec-0.1.5-darwin-arm64.tar.gz"
-  sha256 "757b96190d821df4477874f6dda542e61033c4c3db726b1957ed553cbed4f980"
-  version "0.1.5"
+  url "https://github.com/RobertoBarros/lazyrspec/releases/download/v0.1.6/lazyrspec-0.1.6-darwin-arm64.tar.gz"
+  sha256 "fd00a09e227210400523ae881403a0fc480e06d611b5c7e729485f2cdb97b3ab"
+  version "0.1.6"
   license "MIT"
 
   def install
